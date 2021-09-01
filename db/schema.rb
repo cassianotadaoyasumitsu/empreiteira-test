9606cdf9a0b6901cdd6b74fc2d527d16c20680df
Cassiano Tadao Yasumitsu <cassiano@usuarioincomum.com>
2021-09-01 09:34:25 +0900

    Revert "React installed"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_134401) do
+ActiveRecord::Schema.define(version: 2021_08_30_105151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,13 +34,6 @@ ActiveRecord::Schema.define(version: 2021_08_31_134401) do
     t.date "document_date"
     t.string "passport"
     t.date "passport_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reacts", force: :cascade do |t|
-    t.string "name", null: false
-    t.boolean "is_completed", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
