9fff0e0d3711ce73005aa5cfea5f2c36ad0a81da
Cassiano Tadao Yasumitsu <cassiano@usuarioincomum.com>
2021-09-04 15:28:12 +0900

    Revert "create admin"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
